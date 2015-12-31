745fea51ad2e72bcb12ac9e7af0387553755145d
Christoph <christoph@Christophs-MacBook-Pro.local>
2015-12-31 14:29:07 +0100

    removed test() again, it does not show output on windows

--- R/test.R
+++
@@ -1,9 +0,0 @@
-test <- function(){
-  default.path <- file.path(path.package("x13binary"), "bin")
-  if (.Platform$OS.type == "windows"){    
-    x13.bin <- file.path(default.path, "x13ashtml.exe")
-  } else {
-    x13.bin <- file.path(default.path, "x13ashtml")
-  }
-  system(x13.bin)
-}

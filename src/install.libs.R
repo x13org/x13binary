17d9f57babc078003ef7d987d3cc3f8266b87e06
Jeroen Ooms <jeroenooms@gmail.com>
2024-01-20 17:33:46 +0100

    Fix linking to libfortran on MacOS

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,1 +1,11 @@
 # This file prevents R from looking for a shared library
+if(grepl("darwin", R.Version()$platform)){
+  links <- trimws(readLines(pipe("otool -L ../inst/bin/x13ashtml")))
+  input <- unique(regmatches(links, regexpr("/.*/libgfortran.*dylib", links)))
+  if(length(input)){
+    output <- file.path(R.home('lib'), basename(input))
+    if(file.exists(output)){
+      system2('install_name_tool', c('-change', input, output, "../inst/bin/x13ashtml"))
+    }
+  }
+}
